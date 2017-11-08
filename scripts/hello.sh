30d0e63f38269953f068ae6a2113cdcd04e192bf
Esteve Aguilera <esteve@fewlaps.com>
2017-11-08 09:22:20 +0100

    Add script that opens the apps I use to work

---
+++ scripts/hello.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+say -v "Samantha" "Good morning Steve"
+say -v "Samantha" "Welcome to the jungle"
+open -a SourceTree
+open -a /Applications/Android\ Studio.app
+open -a Slack
+open -a Telegram
